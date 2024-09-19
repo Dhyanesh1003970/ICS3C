e0161a0c4733377b84187f401b53cbf83861a5aa
Dhyanesh1003970 <1003970@pdsb.net>
2024-09-19 10:15:06 -0400

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,10 @@
+print('#1')
+amount = 24
+print('Two doozens is:', amount)
+print('#2')
+amount = 7 * 8
+print('7 x 8 is', amount)
+print('#3')
+amount = 8
+amount = amount * 8
+print('8 x 8 is', amount)
