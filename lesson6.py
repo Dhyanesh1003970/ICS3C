2ebd4278edc8d9d55a6f0b6dc2c9a4ae10c7411f
Dhyanesh1003970 <1003970@pdsb.net>
2024-09-20 08:29:27 -0400

    Create lesson6.py

---
+++ lesson6.py
@@ -0,0 +1,12 @@
+import math
+age = input("What is your age? ")
+age = int(age)
+year = input('What is your birth year? ') 
+year = int(year)
+year = math.pow(year, 2)
+year = year + 5
+year = year * 50
+year = year + age
+year = year - 250
+year = year / 100
+print(year)
