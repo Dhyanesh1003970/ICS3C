55d5cf27b9d34c372d46d45fd168e32be41d30b9
Dhyanesh1003970 <1003970@pdsb.net>
2024-10-04 09:02:52 -0400

    Update U2L6.py

--- U2L6.py
+++ U2L6.py
@@ -1,4 +1,19 @@
+#modification 1
 s = '#'
 for i in range(10,0,-1):
   print(s*i)
   
+#modification 2
+rows = int(input("Enter number of rows: "))
+
+k = 0
+
+for i in range(1, rows+1):
+    for space in range(1,(rows-i)+1):
+        print(end="  ")
+   
+    while k!=(2*i-1):
+        print("# ", end='')
+        k += 1
+    k = 0
+    print()
