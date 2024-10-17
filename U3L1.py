23fa7d17ca1a92b8993eeb47366fb2d2abb11e7b
Dhyanesh1003970 <1003970@pdsb.net>
2024-10-17 09:56:20 -0400

    Create U3L1.py

---
+++ U3L1.py
@@ -0,0 +1,7 @@
+count=0
+s =' '
+program =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in."
+for c in program:
+  if(c==s):
+    count = count + 1
+print('The number of spaces is', count)
