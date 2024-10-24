3c07f0219177178a0e119ae06b8f8528220a6bc4
Dhyanesh1003970 <1003970@pdsb.net>
2024-10-24 09:30:37 -0400

    Update A2.py

--- A2.py
+++ A2.py
@@ -1,21 +1,35 @@
 import math
+d = 'done'
+
+num = (input('Enter the number of photos: '))
+
+while(num != d):
+  
+    x=1
+
+    num = int(num)
+    while(num <= 0):
+        print('Please enter a number greater than zero')
+        num = int(input('Enter the number of photos: '))
+        num = int(num)
+    m = math.floor((math.sqrt(num)))
+    
+
+    t=[]
+    
+    while(x<=num):
+        if (num%x==0):
+            if(x != num):
+                if(x != 1):
+                    if(num / m != m):
+                        print(x)
+                    elif(num / m == m):
+                        t.append(x)
+                        
+                         
+        x = x+1
 
 
-num = int(input('Enter the number of photos: '))
-
-while(num <= 0):
-  print('Please enter a number greater than zero')
-  num = int(input('Enter the number of photos: '))
-
-ma = math.floor(math.sqrt(num))
-x = 1
-
-print(ma, 'ma')
-
-while(ma*x != num):
-  x=x+1
-  #print(x)
-  if(x*ma==num):
-    print(x,'x',ma)
-  else:
-    print('goodbye')
+    if(len(t)==1):
+        print(t)
+    num = (input('Enter the number of photos: '))
