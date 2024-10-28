bd2cb626558c8efb810a524a2388e5bd94951ad8
Dhyanesh1003970 <1003970@pdsb.net>
2024-10-28 09:19:51 -0400

    Update A2.py

--- A2.py
+++ A2.py
@@ -5,7 +5,7 @@ Course: ICS3C0-2
 
 Variable dictionary:
 num = user input(int)
-d = variable for end program(string)
+d = varible for end program(string)
 x = starting loop(int)
 m = square root of num and rounded down(int)
 t = answer of perimeter(int)
@@ -13,12 +13,12 @@ t = answer of perimeter(int)
 
 
 
-print('Hello, welcome to photo organizer, answer the question to receive the dimensions, Enter the word done when you are done with the program')
+print('Hello, welcome to photo organzier, answer the question to recieve the deminsions, Enter the word done when you are done with the program')
 
 print()
 
-import math #math library
-d = 'done' #Variable for end program
+import math #math libaray
+d = 'done' #Varible for end program
 
 num = (input('Enter the number of photos: ')) #user input, variable defined as 'num'
 
@@ -27,7 +27,7 @@ while(num != d): #Will continue to loop until the user input equals 'd'
     x=1 
 
     num = int(num)
-    while(num <= 0): # will continue to loop if user keeps inputting the number<=0
+    while(num <= 0): # will continue to loop if user keeps inputing the number<=0
         print('Please enter a number greater than zero')
         print()
         num = int(input('Enter the number of photos: '))
@@ -39,10 +39,12 @@ while(num != d): #Will continue to loop until the user input equals 'd'
     while(x<=num): #Factoring part of the codes
       
         if(num%m!=0): #Seeing if input is a factor of 'm'
-            m=m+1 #If not, 1 in added to 'm'
+           while(m<num):
+               m=m+1 #If not, 1 in added to 'm'
+        
         
         if (m*x==num):
-            print(m ,'x', x)#dimensions
+            print(m ,'x', x)#deminsions
             t = 2*(m+x) #formula for perimeter
             print('The minimum perimeter would be:', t)
             
@@ -51,4 +53,3 @@ while(num != d): #Will continue to loop until the user input equals 'd'
     
     print()             
     num = (input('Enter the number of photos: '))#loop
-
