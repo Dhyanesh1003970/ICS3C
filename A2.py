860b33658869a1a4326d76cd100a7318657f4b38
Dhyanesh1003970 <1003970@pdsb.net>
2024-10-28 08:54:37 -0400

    Update A2.py

--- A2.py
+++ A2.py
@@ -1,34 +1,52 @@
-import math
-d = 'done'
+''' 
+Name: Dhyanesh Murugesan
+Course: ICS3C0-2
 
-num = (input('Enter the number of photos: '))
 
-while(num != d):
+Variable dictionary:
+num = user input(int)
+d = varible for end program(string)
+x = starting loop(int)
+m = square root of num and rounded down(int)
+t = answer of perimeter(int)
+'''
+
+
+
+print('Hello, welcome to photo organzier, answer the question to recieve the deminsions, Enter ''done'' when you are done with the program')
+
+print()
+
+import math #math libaray
+d = 'done' #Varible for end program
+
+num = (input('Enter the number of photos: ')) #user input, variable defined as 'num'
+
+while(num != d): #Will continue to loop until the user input equals 'd'
   
-    x=1
+    x=1 
 
     num = int(num)
-    while(num <= 0):
+    while(num <= 0): # will continue to loop if user keeps inputing the number<=0
         print('Please enter a number greater than zero')
         num = int(input('Enter the number of photos: '))
-        num = int(num)
-    m = math.floor((math.sqrt(num)))
-    
-   # print(m)
-    
-    
-    
-    while(x<=num):
-        if(num%m!=0):
-            m=m+1
+        num = int(num)#Data change
+    m = math.floor((math.sqrt(num))) #Takes input of user and finds the square root than rounds down
+
+
+  
+    while(x<=num): #Factoring part of the codes
+      
+        if(num%m!=0): #Seeing if input is a factor of 'm'
+            m=m+1 #If not, 1 in added to 'm'
         
         if (m*x==num):
-            print(m ,'x', x)
-            t = 2*(m+x)
+            print(m ,'x', x)#deminsions
+            t = 2*(m+x) #formula for perimeter
             print('The minimum perimeter would be:', t)
             
             
         x = x+1
     
                         
-    num = (input('Enter the number of photos: '))
+    num = (input('Enter the number of photos: '))#loop
