f14ac7f714d60ebddc0658b55c81f962283660d5
Dhyanesh1003970 <1003970@pdsb.net>
2024-11-06 08:40:27 -0500

    Update U3L9.py

--- U3L9.py
+++ U3L9.py
@@ -1,78 +1,44 @@
-L = input("how many lines are you inputing? ")
-
-L =int(L)
-
-
-
-
-c = 'C'
-g = 'G'
-t = 'T'
-a = 'A'
-
-for j in range (L):
-  p=[]
-
-  def comp(dna):
-    
-    c = 'C'
-    g = 'G'
-    t = 'T'
-    a = 'A'
- 
-    for i in range (len(dna)):
-      if(dna[i]==c):
-        G='G'
-        p.append(G)
-     
-      elif(dna[i]==g):
-        C='C'
-        p.append(C)
-     
-      elif(dna[i]==t):
-        A='A'
-        p.append(A)
-     
-      elif(dna[i]==a):
-        T='T'
-        p.append(T)
-
-       
-    print(p)
-    
- 
- 
-  def validate():
-      if(len(p)== len(dna)):
-        print('Valid')
-        
-       
-      else:
-        print('Not valid:', end='')
-        for i in range(len(dna)):
-            if(dna[i]==c):
-              x=1
-    
-            elif(dna[i]==g):
-              x=1
-    
-            elif(dna[i]==t):
-              x=1
-     
-            elif(dna[i]==a):
-              x=1
-          
-            else:
-              j = i+1
-              print(dna[i],'found in position', j )
+def wc(TdegC, windKPH):
+  wTemp=0
   
-
-
+  wTemp = 13.12+0.6215*TdegC
+  wTemp = wTemp - 11.37*windKPH**0.16
+  wTemp = wTemp+0.3965*TdegC*windKPH**0.16
+  
+  if(0 >= wTemp> -10 ):
+    print('Low risk')
+  elif(-10>=wTemp>-28):
+    print('Moderate risk of hypothermia')
+  elif(-28>=wTemp>-40):
+    print('High risk of frostbite')
+  elif(-40>=wTemp>-48):
+    print('Severe risk of frostbite: exposed skin freezes in 5-10 minutes')
+  elif(-48>=wTemp>-55):
+    print('Severe risk of frostbite: exposed skin freezes in 2-5 minutes')
+  else:
+    print('Extreme risk: exposed skin freezes in under 2 minutes')
+  
+  return wTemp
   
   
-  
-  print()     
-  dna = input('Input strand: ')
+T = -5.0
+W = 10.0
 
-  comp(dna) 
-  validate()
+
+print("WC=%8.3f T=%8.3f W=%6.3f km/h" % (wc(T, W), T, W))
+print()
+
+T = -20.0
+W = 20.0
+
+
+
+print("WC=%8.3f T=%8.3f W=%6.3f km/h" % (wc(T, W), T, W))
+print()
+
+T = -45.0
+W = 40.0
+
+print("WC=%8.3f T=%8.3f W=%6.3f km/h" % (wc(T, W), T, W))
+
+print()
