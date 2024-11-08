a762194dc69023d43aa74844f33c0d305d8c3d19
Dhyanesh1003970 <1003970@pdsb.net>
2024-11-08 09:25:54 -0500

    Create U3L11.py

---
+++ U3L11.py
@@ -0,0 +1,18 @@
+import random
+
+def shuff(s):
+    t=[]
+    s +=1
+    g=1
+    for i in range (1,s):
+        x = random.randrange(1, s)
+        
+        
+            t.append(x)
+         g+=1
+        
+        
+    print(t)
+    
+    
+    
