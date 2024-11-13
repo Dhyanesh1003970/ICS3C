b0db5e43f5f32297e1f2c42900658f51c50c2f31
Dhyanesh1003970 <1003970@pdsb.net>
2024-11-13 10:50:07 -0500

    Create U5.L1.py

---
+++ U5.L1.py
@@ -0,0 +1,26 @@
+import turtle
+
+t = turtle.Turtle()
+
+#mod 1
+for i in range (4):
+    t.forward(100)
+    t.left(90)
+    
+    
+#mod 2
+    
+for i in range(3):
+    t.forward(100)
+    t.left(120)
+    
+    
+    
+#mod 3
+turtle.penup()
+turtle.goto(100, 100)
+turtle.dot(20, "red")
+turtle.penup(100)
+
+
+
