734d8d64543e2aaf6b42ed54c1eff3683b3fcbf5
Dhyanesh1003970 <1003970@pdsb.net>
2024-11-18 09:58:58 -0500

    Update u5L2p3.py

--- u5L2p3.py
+++ u5L2p3.py
@@ -13,8 +13,6 @@ print(rows, cols, numColors)
 
 
 
-
-
 for i in range(numColors):
     colorLine = fh.readline()
     colorLine.strip()
@@ -24,27 +22,39 @@ for i in range(numColors):
     
 colorDefs = [[0] * 2] * numColors
 for i in range(numColors):
-    colorline = fh.readline()
+    colorline =fh.readline()
     colorLine.strip()
     sym, c, color = colorLine.split()
     
     if(sym=='~'):
-        sym==' '
+        sym=
+    for i in range(rows):
+        a[i] = fh.readline()
+        print(a[i],end='')
+        
+        
+        
+        
+fh.close()=' '
     
-    colorDefs[i][0] = sym
+    colorDef
+    for i in range(rows):
+        a[i] = fh.readline()
+        print(a[i],end='')
+        
+        
+        
+        
+fh.close()s[i][0] = sym
     colorDefs[i][1] = color
-    
     
     a=[0]*rows
     x=[]
     for i in range(rows):
+        a[i] = fh.readline()
+        print(a[i],end='')
         
-    
-    
-    
-    
+        
+        
+        
 fh.close()
-
-
-        
-
