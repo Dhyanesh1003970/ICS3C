d08436dd67f0794e5a9927e8f9f1fadd983ddeba
Dhyanesh1003970 <1003970@pdsb.net>
2024-12-12 08:44:37 -0500

    Create U5L7.py

---
+++ U5L7.py
@@ -0,0 +1,45 @@
+ar=[]
+
+arr=[]
+
+x = 0
+y = 1
+
+ar.append(x)
+ar.append(y)
+
+arr.append(x)
+arr.append(y)
+
+temp = input('Enter a number: ')
+temp = int(temp)-1
+
+
+
+n=0
+
+
+
+def f(n):
+    
+    if (n==temp):
+        return
+    
+    else:
+        r = len(ar)-2
+        q = len(ar)-1
+
+        x2 =ar[r]
+        y2=ar[q]
+
+        t = x2 + y2
+        arr.append(t)
+        ar.append(t)
+        
+        n+=1
+        f(n)
+        return
+
+f(n)
+
+print(arr)
