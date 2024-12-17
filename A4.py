a152055a2e8da3c6f63763ce797570d99ab49c46
Dhyanesh1003970 <1003970@pdsb.net>
2024-12-16 20:59:57 -0500

    Update A4.py

--- A4.py
+++ A4.py
@@ -20,12 +20,12 @@ for i in range (1038):
     
     mon, dat, yer, wrd=wrd.split()
     
-    words.append(wrd)
-    months.append(mon)
-    date.append(dat)
-    years.append(yer)
+    d = mon, dat, yer
+    date.append(d)
     
-   
+    words.append(wrd)
+    
+
 
 
 
@@ -34,8 +34,7 @@ for i in range(len(words)):
             if (words[i] < words[j]):
                 sort(words, i, j)
                 sort(date, i, j)
-                sort(months, i, j)
-                sort(years, i, j)
+            
                 
                 
 print('Welcome to the Wordle Database')
@@ -44,55 +43,85 @@ wd = input('Enter w if you are looking for a word, or d for a word a certain dat
 if wd == wd.upper():
     wd = wd.lower()
     
+mths=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+mths2=['01','02','03','03','05','06','07','08','09','10','11','12']
 
-mths = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]
+
 
 
 if (wd == 'w'):
     word = input('What word are you looking for? ')
     word = word.upper()
-    print(word)
 else:
-    month=()
     year = input('Enter the year: ')
-    year=int(year)
+    year1=int(year)
+    
     month = input('Enter the month (3-letter abbreviation, as in Jan for January): ')
+     
     Date = input('Enter the date: ')
-    Date = int(Date)
+    Date1 = int(Date)
     
     
-    for i in range(len(mths)):
-        if month == mths:
-            month=mths[i]
     
     
+    
+
+
+            
+                
+    
+    month= month.replace("'", " ")
     dt = month, Date, year
-    print(dt)
+    print(year1, month2, Date1)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+d=[]
+w=[]    
     
 
 if (wd=='d'):
+    for i in range(len(mths)):
+        if(month==mths[i]):
+            month2 = mths2[i]
+    
+    
+    
     
     for i in range(len(date)):
         if dt==date[i]:
-            print(word[i])
+            print(words[i])
+            w.append('1')
+    
+
 
 if (wd=='w'):
+    for i in range(len(mths)):
+        if(month==mths[i]):
+            month2 = mths2[i]
+        
+    
+    
+    
+    
     for i in range(len(words)):
         if(word == words[i]):
+            
+            mnth, day, yar = date[i].strip()
+            for i in range(len(mths)):
+                if(mnth==mths[i]):
+                    mnth = mths2[i]
+            
             print(date[i])
+            d.append('1')
+            
 
+
+if(wd=='w'):
+    if(len(d)==0):
+        print('Word is not in Database')
+
+if(wd=='d'):
+    if(len(w)==0):
+        print('This date is not in Database')
 
 
 fh.close()
