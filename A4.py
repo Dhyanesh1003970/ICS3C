006b8b87135d75029949164411fe97265237d5e6
Dhyanesh1003970 <1003970@pdsb.net>
2024-12-18 09:22:54 -0500

    Update A4.py

--- A4.py
+++ A4.py
@@ -4,6 +4,73 @@ fh=open(filename, 'r')
 
 
 
+def merge(arr, l, m, r, date):
+    n1 = m - l + 1
+    n2 = r - m
+    
+    L = [0] * (n1)
+    R = [0] * (n2)
+    L2 = [0] * (n1)
+    R2 = [0] * (n2)
+    
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+        L2[i] = date[l+i]
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+        R2[j] = date[m+1+j]
+    
+    i = 0  
+    j = 0  
+    k = l  
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            date[k] = L2[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            date[k]= R2[j]
+            j += 1
+        k += 1
+    
+ 
+    while i < n1:
+        arr[k] = L[i]
+        date[k] = L2[i]
+        i += 1
+        k += 1
+    
+   
+    while j < n2:
+        arr[k] = R[j]
+        date[k]=R2[j]
+        j += 1
+        k += 1
+
+
+
+
+
+
+
+def mergesort(arr, l , r, date):
+    
+    if (l<r):
+        m = l + (r-l)//2
+        mergesort(arr , l , m, date)
+        mergesort(arr, m+1, r, date)
+        merge(arr, l , m , r, date)
+
+
+
+
+
+
+
+
+
+
 
 def merge1(p,q,r):
     mths=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -35,12 +102,6 @@ def merge1(p,q,r):
 
 
 
-def sort(X, i, j):
-    y = X[i]
-    X[i] = X[j]
-    X[j] = y
-
-
 
 date=[]
 words=[]
@@ -58,19 +119,11 @@ for i in range (1038):
 l = len(words)
 
 
+mergesort(words, 0, l-1, date)
 
 
 
 
-
-
-
-
-for i in range(len(words)):
-        for j in range(len(words)):
-            if (words[i] < words[j]):
-                sort(words, i, j)
-                sort(date, i, j)
             
                 
                 
@@ -91,7 +144,7 @@ w=[]
 
 if (wd == 'w'):
     m=[]
-    print('-----------------------------------------------------------------')
+    print('------------------------------------------------------------------------')
     word = input('What word are you looking for? ')
     word = word.upper()
 
@@ -128,10 +181,10 @@ if (wd == 'w'):
             
 else:
     
-    print('-----------------------------------------------------------------')
+    print('------------------------------------------------------------------------')
     year = input('Enter the year: ')
     year1=int(year)
-    print('---------------------------------')
+    print('--------------------')
     month = input('Enter the month (3-letter abbreviation, as in Jan for January): ')
     print('---------------------------------')
     Date = input('Enter the date: ')
@@ -164,11 +217,14 @@ else:
 
 if(wd=='w'):
     if(len(d)==0):
-        print('Word is not in Database')
+        print()
+        print('Word is not in Database, please check agian')
 
 if(wd=='d'):
     if(len(w)==0):
-        print('This date is not in Database')
+        print()
+        print('This date is not in Database', end='')
+        print(', please check the date and rerun program,(check the capital letter on month)')
 
 
 fh.close()
